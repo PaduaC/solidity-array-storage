2de265dc7df4bbf8818605e54de584219ff7f971
cpadua912 <cristian.padua912@gmail.com>
2021-01-13 01:40:02 -0500

    configured web3

--- client/index.js
+++ client/index.js
@@ -6,13 +6,10 @@ let advancedStorage;
 
 const initWeb3 = () => {
   return new Promise((resolve, reject) => {
-    // Case 1: old metamask is present
-    if (window.web3 !== "undefined") {
-      return resolve(new Web3(window.web3.currentProvider));
-    }
-    // Case 2: new metamask is present
-    if (window.ethereum !== "undefined") {
-      window.ethereum.enable
+    // Case 1: new metamask is present
+    if (typeof window.ethereum !== "undefined") {
+      window.ethereum
+        .enable()
         .then(() => {
           resolve(new Web3(window.ethereum));
         })
@@ -21,14 +18,53 @@ const initWeb3 = () => {
         });
       return;
     }
+    // Case 2: old metamask is present
+    if (typeof window.web3 !== "undefined") {
+      return resolve(new Web3(window.web3.currentProvider));
+    }
+
     // Case 3: no metamask is present, connect to Ganache
     resolve(new Web3("http://localhost:9545"));
   });
 };
 
-const initContract = () => {};
+const initContract = () => {
+  const deploymentKey = Object.keys(AdvancedStorage.networks)[0];
+  return new web3.eth.Contract(
+    AdvancedStorage.abi,
+    AdvancedStorage.networks[deploymentKey].address
+  );
+};
 
-const initApp = () => {};
+const initApp = () => {
+  const $addData = document.getElementById("addData");
+  const $data = document.getElementById("data");
+  let accounts = [];
+
+  web3.eth
+    .getAccounts()
+    .then((_accounts) => {
+      accounts = _accounts;
+      return advancedStorage.methods.getAll().call();
+    })
+    .then((result) => {
+      $data.innerHTML = result.join(", ");
+    });
+
+  $addData.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const data = e.target.elements[0].value;
+    advancedStorage.methods
+      .add(data)
+      .send({ from: accounts[0] })
+      .then(() => {
+        return advancedStorage.methods.getAll().call();
+      })
+      .then((result) => {
+        $data.innerHTML = result.join(", ");
+      });
+  });
+};
 
 document.addEventListener("DOMContentLoaded", () => {
   initWeb3()
